b42d9856f805c32bd3045db4f30b231f66b1b7e8
malinoile <filippov.malinoil@yandex.ru>
2020-12-29 21:00:07 +1000

    Домашнее задание 5 урока

---
+++ src/homework/lesson05/animals/Horse.java
@@ -0,0 +1,8 @@
+package homework.lesson05.animals;
+
+public class Horse extends Animal {
+
+    public Horse(float maxRunDistance, float maxSwimDistance, float maxJumpHeight, String name) {
+        super(maxRunDistance, maxSwimDistance, maxJumpHeight, name);
+    }
+}
